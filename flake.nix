51d2c8c310a77f3da7d0984e800964e4f8428516
pilz0 <marie0@riseup.net>
2025-12-23 13:24:39 +0100

    joooonge

--- flake.nix
+++ flake.nix
@@ -1,12 +1,33 @@
 {
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.gradle2nix.url = "github:tadfisher/gradle2nix/v2";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
-    { self, gradle2nix }:
     {
-      packages.x86_64-linux.default = gradle2nix.builders.x86_64-linux.buildGradlePackage {
+      self,
+      nixpkgs,
+      gradle2nix,
+      flake-utils,
+    }:
+    {
+      name = "freaklog";
+
+      devShells.x86_64-linux.default =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        pkgs.mkShell {
+          #          JAVA_HOME = "/nix/store/mibyr0q42jihn6i0xrda79bljkfyrhfs-android-studio-stable-2025.2.1.8-unwrapped/jbr";
+          packages = with pkgs; [
+            git
+            jetbrains.jdk
+          ];
+        };
+
+      app = gradle2nix.builders.x86_64-linux.buildGradlePackage {
         pname = "app";
-        version = "11.11";
+        version = "11.12";
         lockFile = ./gradle.lock;
       };
     };
