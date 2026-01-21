de0870fd51f393e2e5467b8b09e409b127b319f7
pilz <marie0@riseup.net>
2026-01-21 10:36:01 +0100

    try to optimise pipelines

--- flake.nix
+++ flake.nix
@@ -39,9 +39,9 @@
       in
       {
         packages = {
-          default = self.packages.${system}.apk;
-          apk = gradle2nix.builders.${system}.buildGradlePackage {
-            pname = "freaklog apk";
+          default = self.packages.${system}.apk-release;
+          apk-release = gradle2nix.builders.${system}.buildGradlePackage {
+            pname = "freaklog apk release";
             version = "11.13";
             lockFile = ./gradle.lock;
             src = ./.;
@@ -78,8 +78,46 @@
               cp app/build/outputs/apk/release/*.apk $out/bin/
             '';
           };
+          apk-debug = gradle2nix.builders.${system}.buildGradlePackage {
+            pname = "freaklog apk debug";
+            version = "11.13";
+            lockFile = ./gradle.lock;
+            src = ./.;
+            gradleBuildFlags = [ "assembleDebug" ];
+            ANDROID_HOME = "${androidSdk.androidsdk}/libexec/android-sdk";
+            preBuild = ''
+              rm -f local.properties
+              export ANDROID_USER_HOME=$(mktemp -d)
+              mkdir -p $ANDROID_USER_HOME/.android
+              export GRADLE_OPTS="-Djava.io.tmpdir=$ANDROID_USER_HOME/tmp"
+              mkdir -p $ANDROID_USER_HOME/tmp
+            '';
+            nativeBuildInputs = [
+              pkgs.jre17_minimal
+              androidSdk.androidsdk
+            ];
+            overrides = {
+               "com.android.tools.build:aapt2:8.13.2-14304508" = {
+                 "aapt2-8.13.2-14304508-linux.jar" = src: pkgs.runCommandCC src.name {
+                  nativeBuildInputs = [ pkgs.jdk pkgs.libgcc pkgs.autoPatchelfHook ];
+                  dontAutoPatchelf = true;
+                } ''
+                  cp ${src} aapt2.jar
+                  jar xf aapt2.jar aapt2
+                  chmod +x aapt2
+                  autoPatchelf aapt2
+                  jar uf aapt2.jar aapt2
+                  cp aapt2.jar $out
+                '';
+              };
+            };
+            installPhase = ''
+              mkdir -p $out/bin
+              cp app/build/outputs/apk/debug/*.apk $out/bin/
+            '';
+          };
            aab = gradle2nix.builders.${system}.buildGradlePackage {
-             pname = "freaklog aab";
+             pname = "freaklog android app bundle";
              version = "11.13";
              lockFile = ./gradle.lock;
              src = ./.;
@@ -113,11 +151,10 @@
              };
              installPhase = ''
                mkdir -p $out/bin
-               cp app/build/outputs/bundle/release/*.aab $out/bin/
-               cp app/build/outputs/bundle/debug/*.aab $out/bin/
+               cp app/build/outputs/bundle/release/app-release.aab $out/bin/app-release-unsigned.aab
+               cp app/build/outputs/bundle/debug/app-debug.aab $out/bin/app-debug-unsigned.aab
              '';
            };
-
         };
 
         devShells.default = pkgs.mkShell {
