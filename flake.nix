1fafd66e1d26d7cc6258d0232c257b43db8b8f98
pilz <marie0@riseup.net>
2026-01-21 10:42:21 +0100

    fix flake.nix

--- flake.nix
+++ flake.nix
@@ -39,8 +39,8 @@
       in
       {
         packages = {
-          default = self.packages.${system}.apk-release;
-          apk-release = gradle2nix.builders.${system}.buildGradlePackage {
+          default = self.packages.${system}.apk;
+          apk = gradle2nix.builders.${system}.buildGradlePackage {
             pname = "freaklog apk release";
             version = "11.13";
             lockFile = ./gradle.lock;
@@ -76,44 +76,6 @@
             installPhase = ''
               mkdir -p $out/bin
               cp app/build/outputs/apk/release/*.apk $out/bin/
-            '';
-          };
-          apk-debug = gradle2nix.builders.${system}.buildGradlePackage {
-            pname = "freaklog apk debug";
-            version = "11.13";
-            lockFile = ./gradle.lock;
-            src = ./.;
-            gradleBuildFlags = [ "assembleDebug" ];
-            ANDROID_HOME = "${androidSdk.androidsdk}/libexec/android-sdk";
-            preBuild = ''
-              rm -f local.properties
-              export ANDROID_USER_HOME=$(mktemp -d)
-              mkdir -p $ANDROID_USER_HOME/.android
-              export GRADLE_OPTS="-Djava.io.tmpdir=$ANDROID_USER_HOME/tmp"
-              mkdir -p $ANDROID_USER_HOME/tmp
-            '';
-            nativeBuildInputs = [
-              pkgs.jre17_minimal
-              androidSdk.androidsdk
-            ];
-            overrides = {
-               "com.android.tools.build:aapt2:8.13.2-14304508" = {
-                 "aapt2-8.13.2-14304508-linux.jar" = src: pkgs.runCommandCC src.name {
-                  nativeBuildInputs = [ pkgs.jdk pkgs.libgcc pkgs.autoPatchelfHook ];
-                  dontAutoPatchelf = true;
-                } ''
-                  cp ${src} aapt2.jar
-                  jar xf aapt2.jar aapt2
-                  chmod +x aapt2
-                  autoPatchelf aapt2
-                  jar uf aapt2.jar aapt2
-                  cp aapt2.jar $out
-                '';
-              };
-            };
-            installPhase = ''
-              mkdir -p $out/bin
-              cp app/build/outputs/apk/debug/*.apk $out/bin/
             '';
           };
            aab = gradle2nix.builders.${system}.buildGradlePackage {
